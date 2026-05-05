f7f4809945307f876e89daa338cf70fe8e5808db
yamurgun <guneryagmurr@gmail.com>
2026-05-05 22:20:44 +0300

    finished encoder

--- morse/encoder.py
+++ morse/encoder.py
@@ -14,7 +14,13 @@ def encode(text):
     Encodes the given text into Morse code.
     Words are separated by a pipe (|) and letters by a space.
     """
-    pass  # YOUR CODE HERE
+    words = text.split()
+    encoded_words = []
+
+    for word in words:
+        encoded_words.append(encode_word(word))
+
+    return "|".join(encoded_words)
 
 
 def encode_word(word):
@@ -22,7 +28,13 @@ def encode_word(word):
     Encodes a single word into Morse code.
     Letters are separated by a space.
     """
-    pass  # YOUR CODE HERE
+    letters = []
+
+    for char in word:
+        if char.upper() in MORSE:
+            letters.append(MORSE[char.upper()])
+
+    return " ".join(letters)
 
 
 if __name__ == "__main__":
