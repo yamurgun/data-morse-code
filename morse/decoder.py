53c3d1dda17cda02654e83469ea6c0e8288b9bff
yamurgun <guneryagmurr@gmail.com>
2026-05-05 23:29:37 +0300

    Coded decoder and altered example word and sentence

--- morse/decoder.py
+++ morse/decoder.py
@@ -1,9 +1,32 @@
-pass  # YOUR CODE HERE
+from morse.mapping import MORSE
+REVERSE_MORSE = {v: k for k, v in MORSE.items()}
 
+def decode_word(word):
+    letters = word.split()
+    decoded = []
+
+    for letter in letters:
+        if letter in REVERSE_MORSE:
+            decoded.append(REVERSE_MORSE[letter])
+
+    return "".join(decoded)
+
+def decode(text):
+    words = text.split("|")
+    decoded_words = []
+
+    for word in words:
+        decoded_words.append(decode_word(word))
+
+    return " ".join(decoded_words)
 
 if __name__ == "__main__":
     # Example usage for one word
-    pass  # YOUR CODE HERE
+    EXAMPLE_MORSE = ".... .."
+    DECODED_TEXT = decode_word(EXAMPLE_MORSE)
+    print(f"Decoded '{EXAMPLE_MORSE}' to text: '{DECODED_TEXT}'")
 
     # Example usage for one sentence
-    pass  # YOUR CODE HERE
+    EXAMPLE_MORSE = ".... ..|--. ..- -.-- ..."
+    DECODED_TEXT = decode(EXAMPLE_MORSE)
+    print(f"Decoded '{EXAMPLE_MORSE}' to text '{DECODED_TEXT}'")
